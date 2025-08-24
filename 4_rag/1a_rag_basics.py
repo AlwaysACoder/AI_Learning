a8ab25bb89df220a5896bc0301c818a311582c1b
Murray Kristell <murraykristell@outlook.com>
2025-08-23 22:43:40 -0400

    Replaced deprecated ChromaDB with up-to-date version

--- 4_rag/1a_rag_basics.py
+++ 4_rag/1a_rag_basics.py
@@ -1,7 +1,7 @@
 import os
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 
 # Define the directory containing the text file and the persistent directory
